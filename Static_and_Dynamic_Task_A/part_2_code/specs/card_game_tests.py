9b857e722a0a1897e4998ecc8e74f060f4c97aa8
Jennifer <jng-96@hotmail.co.uk>
2023-05-13 17:44:09 +0100

    check for ace working

--- Static_and_Dynamic_Task_A/part_2_code/specs/card_game_tests.py
+++ Static_and_Dynamic_Task_A/part_2_code/specs/card_game_tests.py
@@ -6,11 +6,17 @@ class TestCardGame(unittest.TestCase):
 
     def setUp(self):
         self.first_card = Card("club", 6)
+        self.second_card = Card("heart", 1)
     
-    def test_check_for_ace(self):
+    def test_check_not_an_ace(self):
         expected = False
-        actual = self.check_for_ace
+        actual = CardGame.check_for_ace(self, self.first_card)
         self.assertEqual(expected, actual)
+
+    def test_check_for_ace(self):
+        expected = True
+        actual = CardGame.check_for_ace(self, self.second_card)
+        self.assertEqual(expected, actual)   
 
 
 
